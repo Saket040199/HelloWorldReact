628ab9b7f00ab8d7aa1042360d41ea79593d7d27
saket <saketj420@gmail.com>
2021-06-29 17:01:05 +0530

    BAsic helloworld and image added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React from 'react';
+import logo from './asset/addresspic.png';
+
+// class App extends React.Component{
+//   render(){
+//     return (
+//       <div>
+//         <h1>Hello From BridgeLabz</h1>
+//       </div>
+//     );
+//   }
+// }
+
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      
+      title: 'hello from BridgeLabz'
+    }
+  }
+  render(){
+    return(
+      <div className = 'menu'>
+        <h1>{this.state.title}</h1>
+        <img  src={logo} alt="Invalid pic" />
+      </div>
+    )
+  }
 }
 
 export default App;
