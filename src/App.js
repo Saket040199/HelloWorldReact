b738d0b80243bc120933870395d4a1bac116235e
saket <saketj420@gmail.com>
2021-07-01 13:06:39 +0530

    Cretaing new component and route it

--- src/App.js
+++ src/App.js
@@ -1,69 +1,24 @@
-
+import React from 'react'
 import './App.css';
-import React from 'react';
-import logo from './asset/bl.logo.png';
+import Home from './components/home/home';
 
-// class App extends React.Component{
-//   render(){
-//     return (
-//       <div>
-//         <h1>Hello From BridgeLabz</h1>
-//       </div>
-//     );
-//   }
-// }
+import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-class App extends React.Component{
-  url = "https://www.bridgelabz.com/";
-  constructor(){
-    super()
-    this.state = { 
-      userName: '',
-      nameError: ''
-    }
-  }
-  onClick = ($event) => {
-    window.open(this.url,"_blank");
-  }
+class App extends React.Component {
 
-  onNameChange = ($event) => {
-    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
-    this.setState({ userName: $event.target.value})
-    if (nameRegex.test($event.target.value || this.setState.userName==='')){
-      this.setState({ nameError: ''})
-    } else{
-      this.setState({nameError: 'Name is Incorrect'})
-    }
-  }
-  render(){
-    return(
-      <div className="app-main">
-      <div className ="main">
-      <div>
-        <h1>Hello {this.state.userName} from BridgeLabz</h1>
-        <img  src={logo} onClick={this.onClick} alt="Invalid pic" />
+  render() {
+    return (
+      <div className = "app-main" >
+       <Router>
+        <Switch>
+          <Route path="">
+            <Home />
+          </Route>
+        </Switch>
+       </Router>
       </div>
-      <div className='text-box'>
-        <input onChange = {this.onNameChange} />
-        <span className="error-output">{this.state.nameError}</span>
-      </div>
-      <div>
-        At BrideLabz we're a community of
-        <ul>
-          <li> Technologists</li>
-          <li> Thinkers</li>
-          <li> Builders</li>
-        </ul>
-        Working Together.......
-        <br />
-        <br />
-        To know ,visit <a href='https://www.bridgelabz.com/' rel="noreferrer" target="_blank">BrideLabz</a>
-        <br /> i.e <b>Employebility to all</b>
-        </div>
-      </div>
-      </div>
-    )
+    );
   }
 }
 
-export default App;
+ export default App;
---
+++ src/components/home/home.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import logo from '../../asset/bl.logo.png';
+import './home.css';
+
+// class App extends React.Component{
+//   render(){
+//     return (
+//       <div>
+//         <h1>Hello From BridgeLabz</h1>
+//       </div>
+//     );
+//   }
+// }
+
+class Home extends React.Component{
+  url = "https://www.bridgelabz.com/";
+  constructor(){
+    super()
+    this.state = { 
+      userName: '',
+      nameError: ''
+    }
+  }
+  onClick = ($event) => {
+    window.open(this.url,"_blank");
+  }
+
+  onNameChange = ($event) => {
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    this.setState({ userName: $event.target.value})
+    if (nameRegex.test($event.target.value || this.setState.userName==='')){
+      this.setState({ nameError: ''})
+    } else{
+      this.setState({nameError: 'Name is Incorrect'})
+    }
+  }
+  render(){
+    return(
+      <div className="app-main">
+      <div className ="main">
+      <div>
+        <h1>Hello {this.state.userName} from BridgeLabz</h1>
+        <img  src={logo} onClick={this.onClick} alt="Invalid pic" />
+      </div>
+      <div className='text-box'>
+        <input onChange = {this.onNameChange} />
+        <span className="error-output">{this.state.nameError}</span>
+      </div>
+      <div>
+        At BrideLabz we're a community of
+        <ul>
+          <li> Technologists</li>
+          <li> Thinkers</li>
+          <li> Builders</li>
+        </ul>
+        Working Together.......
+        <br />
+        <br />
+        To know ,visit <a href='https://www.bridgelabz.com/' rel="noreferrer" target="_blank">BrideLabz</a>
+        <br /> i.e <b>Employebility to all</b>
+        </div>
+      </div>
+      </div>
+    )
+  }
+}
+
+export default Home;
